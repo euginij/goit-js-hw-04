c19d654a8ea5be195d652d2c1a640d1dff6bac60
euginij <euginij777@gmail.com>
2026-03-13 13:03:14 +0200

    1

---
+++ js/task-3.js
@@ -0,0 +1,30 @@
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  updatePlayTime(hours) {
+    const value = this.playTime + hours;
+    this.playTime = value;
+  },
+  getInfo() {
+   return `${profile.username} has ${profile.playTime} active hours!`;
+ }
+};
+
+
+
+
+
+
+
+
+
+console.log(profile.getInfo());
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); 
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); 
